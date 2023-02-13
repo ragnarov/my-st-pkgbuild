e2e38478b2fd14df22288eac16b8c4c05943cdc3
ragnarov <101@mailu.io>
2023-02-14 03:32:54 +0600

    Changes to be committed:       modified:   config.h

--- config.h
+++ config.h
@@ -204,25 +204,23 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD  (ControlMask|ShiftMask)
-#define KEYCOMB1 (ControlMask|Mod1Mask)
-
-
+#define TERMMOD0 (ControlMask|Mod1Mask)
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ KEYCOMB1,             XK_c,           clipcopy,       {.i =  0} },
-	{ KEYCOMB1,             XK_v,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ TERMMOD0,             XK_Prior,       zoom,           {.f = +1} },
+	{ TERMMOD0,             XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD0,             XK_Home,        zoomreset,      {.f =  0} },
+	{ TERMMOD0,             XK_c,           clipcopy,       {.i =  0} },
+	{ TERMMOD0,             XK_v,           clippaste,      {.i =  0} },
+	{ TERMMOD0,             XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD0,             XK_Num_Lock,    numlock,        {.i =  0} },
 	{ Mod4Mask,             XK_l,           copyurl,        {.i =  0} },
+        { TERMMOD0,             XK_b,           keyboard_select,{.i =  0} },
 };
 
 /*
